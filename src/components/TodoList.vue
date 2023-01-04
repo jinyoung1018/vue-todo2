e2608f094a79b3cdcf2967a57c337f6eb5811e03
jinyoung1018 <jywsb@1816@gmail.com>
2023-01-04 20:03:14 +0900

    remove button

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <ul>
-        <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+        <li v-for="todoItem in todoItems" v-bind:key="todoItem" class='shdow'>
             {{todoItem}}
+            <span class="removeBtn" v-on:click="removeTodo">
+                <i class="fa-solid fa-trash-can"></i>
+            </span>
         </li>
 
 
@@ -16,6 +19,12 @@ export default {
         return{
             todoItems: []
         }
+    },
+    methods: {
+        removeTodo: function(){
+            
+        }
+
     },
     created: function(){
         if (localStorage.length > 0){
@@ -32,6 +41,37 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+ul {
+	list-style-type: none;
+	padding-left: 0px;
+	margin-top: 0;
+	text-align: left;
+}
+li {
+	display: flex;
+	min-height: 50px;
+	height: 50px;
+	line-height: 50px;
+	margin: 0.5rem 0;
+	padding: 0 0.9rem;
+	background: white;
+	border-radius: 5px;
+}
+.checkBtn {
+	line-height: 45px;
+	color: #62acde;
+	margin-right: 5px;
+}
+.checkBtnComplated {
+	color: #b3adad;
+}
+.textCompleted {
+	text-decoration: line-through;
+	color: #b3adad;
+}
+.removeBtn {
+	margin-left: auto;
+	color: #de4343;
+}
 </style>
