460ee81926bd806aa7004b3eb61713eba371dee0
jinyoung1018 <jywsb1816@gmail.com>
2023-01-07 14:17:20 +0900

    store

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,25 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex);
 
+const storage = {
+    fetch(){
+        const arr = [];
+            if (localStorage.length > 0){
+                for(let i = 0; i < localStorage.length ; i ++){
+                    if(localStorage.key(i) != 'loglevel:webpack-dev-server'){
+                        this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));//JSON.parse()스트링을 다시 오브젝트로
+                    }
+                }
+            }  
+        return arr;
+    },
+}
+
 export const store = new Vuex.Store({
+    state:{
+      todoItems: storage.fetch()
+        
+
+    }
 
 });
